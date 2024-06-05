2d339269b7ba18274079807fbbacccd413eb534d
christian kocevar <christian.kocevar@qualitestgroup.com>
2024-06-05 13:51:14 +0200

    first commit

--- index.js
+++ index.js
@@ -11,6 +11,71 @@ app.use(express.urlencoded({extended: false}));
 
 // routes
 app.use("/api/products", productRoute);
+//form fields
+app.post("/api/products",(req,res) => {
+    var eindat= req.body.eindat
+    var entdat=req.body.entdat
+    var verdat=req.body.verdat
+    var autor=req.body.autor
+    var keyword=req.body.keyword
+    var kwp=req.body.kwp
+    var verlag=req.body.verlag
+    var seiten=req.body.seiten
+    var genre=req.body.genre
+    var gengenau=req.body.gengenau
+    var kontinent=req.body.kontinent
+    var land=req.body.land
+    var thema0=req.body.thema0
+    var thema1=eq.body.thema1
+    var thema2=req.body.thema2
+    var zeit=req.body.zeit
+    var amalin=req.body.amalin
+    var position=req.body.position
+    var kontinent=req.body.kontinent
+    var land=req.body.land
+    var thema0=req.body.thema0
+    var thema1=eq.body.thema1
+    var thema2=req.body.thema2
+
+    var amalin
+    var
+        var
+        var
+        var
+
+    position
+    mark
+    markrang
+    topw:
+    belber
+    gesw
+    zustand
+
+    var data={
+        "eindat":eindat,
+        "entdat":entdat,
+        "verdat":verdat,
+        "autor":autor,
+        "keyword":keyword,
+        "kwp":kwp
+        "verlag":verlag,
+        "seiten":seiten,
+        "genre""=req.body.genre
+        "gengenau"=req.body.gengenau
+        "kontinent":kontinent
+        "land":land,
+        "thema0"=req.body.thema0
+        "thema1"=eq.body.thema1
+        "thema2"=req.body.thema2
+        "zeit":zeit,
+        "amalin""=req.body.amalin
+        "position"=req.body.position
+        "kontinent":kontinent
+        "land":land,
+        "thema0"=req.body.thema0
+        "thema1"=eq.body.thema1
+
+    }
 
 
 
